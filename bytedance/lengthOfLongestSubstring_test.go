ef9f3ed14c43c79c710ff7d2c6671b6f834646ba
wangxuewei10 <wangxuewei15@jd.com>
2024-05-08 14:57:44 +0800

    fix

--- bytedance/lengthOfLongestSubstring_test.go
+++ bytedance/lengthOfLongestSubstring_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestLengthOfLongestSubstring(t *testing.T) {
 	testStr := "abba"
-	//maxLen := LengthOfLongestSubstring(testStr)
-	//fmt.Println(maxLen)
+	maxLen := LengthOfLongestSubstring(testStr)
+	fmt.Println(maxLen)
 	maxlen2 := LengthOfLongestSubstring2(testStr)
 	fmt.Println(maxlen2)
 }
