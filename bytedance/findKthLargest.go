89078c1421ab87d564f8e92f263fc732b259ce55
wangxuewei10 <wangxuewei15@jd.com>
2024-07-17 21:24:10 +0800

    feat：findklargest

--- bytedance/findKthLargest.go
+++ bytedance/findKthLargest.go
@@ -2,46 +2,38 @@ package bytedance
 
 import "fmt"
 
-func findKthLargest(nums []int, k int) {
-
+func findKthLargest(nums []int, k int) int {
+	return Partition(nums, k)
 }
 
-func find(nums []int, left, right, k int) int {
-	if left == right {
-		return nums[k]
-	}
-
-	i := left
-	j := right
-	index := nums[l]
+func Partition(nums []int, k int) int { //快排
+	i, j := 0, len(nums)-1
+	pivot := nums[0]
 	for i < j {
-		for i++; nums[i]<
-	}
-
-}
-
-
-func quickselect(nums []int, l, r, k int) int{
-	if (l == r){
-		return nums[k]
-	}
-	partition := nums[l]
-	i := l - 1
-	j := r + 1
-	for (i < j) {
-		for i++;nums[i]<partition;i++{}
-		for j--;nums[j]>partition;j--{}
-		if (i < j) {
-			nums[i],nums[j]=nums[j],nums[i]
+		for i < j && nums[j] < pivot {
+			j--
+		}
+		if i < j {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+		}
+		for i < j && nums[i] > pivot {
+			i++
+		}
+		if i < j {
+			nums[i], nums[j] = nums[j], nums[i]
+			j--
 		}
 	}
-	if (k <= j){
-		return quickselect(nums, l, j, k)
-	}else{
-		return quickselect(nums, j + 1, r, k)
+
+	if i+1 == k {
+		return pivot //   位子恰好就是K-1返回结果
+	} else if i+1 < k {
+		return Partition(nums[i+1:], k-i-1) //以pivot为断点，在后半部分数组中进行查找
+	} else {
+		return Partition(nums[:i], k) //在前半部分数组中进行查找
 	}
 }
-
 
 func quickSort(start, end int, arr []int) {
 	i := start
