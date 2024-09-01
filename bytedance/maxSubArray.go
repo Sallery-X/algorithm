c8fb931c72803e076efa0bfb4841914c779a7d44
0xsallery <0xsallery@pingpong.build>
2024-09-01 21:37:59 +0800

    feat: add max sub array

---
+++ bytedance/maxSubArray.go
@@ -0,0 +1,13 @@
+package bytedance
+
+import "math"
+
+func MaxSubArray(nums []int) int {
+	pre := 0
+	maxA := nums[0]
+	for _, v := range nums {
+		pre = int(math.Max(float64(pre+v), float64(v)))
+		maxA = int(math.Max(float64(maxA), float64(pre)))
+	}
+	return maxA
+}
