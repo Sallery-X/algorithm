c93ca923fbe859efbab9190a8dcb67bc7e841deb
wangxuewei10 <wangxuewei15@jd.com>
2024-09-03 12:37:32 +0800

    add z lever order

---
+++ bytedance/zLevelOrderTraversal.go
@@ -0,0 +1,43 @@
+package bytedance
+
+import "container/list"
+
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	stack := list.New()
+	res := make([][]int, 0)
+	stack.PushBack(root)
+	director := true
+	for stack.Len() != 0 {
+		size := stack.Len()
+		level := make([]int, 0)
+		for i := 0; i < size; i++ {
+			node := stack.Front()
+			level = append(level, node.Value.(*TreeNode).Val)
+			stack.Remove(node)
+			if !director {
+				if node.Value.(*TreeNode).Left != nil {
+					stack.PushBack(node.Value.(*TreeNode).Left)
+				}
+				if node.Value.(*TreeNode).Right != nil {
+					stack.PushBack(node.Value.(*TreeNode).Right)
+				}
+			}
+			if director {
+				if node.Value.(*TreeNode).Right != nil {
+					stack.PushBack(node.Value.(*TreeNode).Right)
+				}
+				if node.Value.(*TreeNode).Left != nil {
+					stack.PushBack(node.Value.(*TreeNode).Left)
+				}
+			}
+		}
+		director = !director
+		res = append(res, level)
+	}
+
+	return res
+
+}
