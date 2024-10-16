865825ecc07c7d97d97aaf87152247cd32eea802
wangxuewei10 <wangxuewei15@jd.com>
2024-10-16 16:55:52 +0800

    add addtwosum of list

---
+++ bytedance/addTwoList_test.go
@@ -0,0 +1,26 @@
+package bytedance
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_addTwoNumbers(t *testing.T) {
+	listA := ListNode{3, &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val:  2,
+			Next: nil,
+		},
+	}}
+
+	listB := ListNode{5, &ListNode{
+		Val: 2,
+		Next: &ListNode{
+			Val:  2,
+			Next: nil,
+		},
+	}}
+
+	fmt.Println(addTwoNumbers(&listA, &listB))
+}
