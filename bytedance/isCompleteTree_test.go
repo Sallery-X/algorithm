57ec0333f26ab635bb75599796b75edfb3236822
wangxuewei10 <wangxuewei15@jd.com>
2024-10-16 11:42:32 +0800

    add iscompleteTree

---
+++ bytedance/isCompleteTree_test.go
@@ -0,0 +1,21 @@
+package bytedance
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_isCompleteTree(t *testing.T) {
+	node := TreeNode{
+		Val: 0,
+		Left: &TreeNode{
+			Val:   1,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val:  2,
+			Left: nil,
+		},
+	}
+	fmt.Println(isCompleteTree(&node))
+}
