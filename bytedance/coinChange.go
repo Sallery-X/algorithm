5c1f80e582205d52436336be4b57908a71d84e3f
wangxuewei10 <wangxuewei15@jd.com>
2024-10-28 18:01:08 +0800

    add

---
+++ bytedance/coinChange.go
@@ -0,0 +1,25 @@
+package bytedance
+
+import "math"
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+
+	for v := range dp {
+		dp[v] = math.MaxInt32
+	}
+	dp[0] = 0
+	for i := 1; i <= amount; i++ {
+		for j := 0; j < len(coins); j++ {
+			if i-coins[j] >= 0 {
+				dp[i] = int(math.Min(float64(dp[i]), float64(dp[i-coins[j]]+1)))
+			}
+		}
+	}
+	if dp[amount] == math.MaxInt32 {
+		return -1
+	}
+
+	return dp[amount]
+
+}
