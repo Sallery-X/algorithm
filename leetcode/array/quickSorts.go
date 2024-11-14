09453a51e92b5c911b17c973e6e63f2bff2831b6
wangxuewei10 <wangxuewei15@jd.com>
2024-11-14 18:09:58 +0800

    add

--- leetcode/array/quickSorts.go
+++ leetcode/array/quickSorts.go
@@ -1,34 +1,36 @@
 package array
 
+import "fmt"
+
 func sort1(nums []int) {
-
 	quickSort1(nums, 0, len(nums)-1)
-
 }
 
 func quickSort1(nums []int, start, end int) {
 	if start >= end {
 		return
 	}
-
-	i := start
-	j := end
-
+	i, j := start, end
 	base := nums[start]
+
 	for i < j {
-		for nums[j] >= base && i < j {
+		//从右向左搞
+		for i < j && nums[j] <= base {
 			j--
 		}
-
-		for nums[i] <= base && i < j {
+		for i < j && nums[i] >= base {
 			i++
 		}
 		if i < j {
 			nums[i], nums[j] = nums[j], nums[i]
 		}
 	}
+	fmt.Println(nums)
+
 	nums[i], nums[start] = nums[start], nums[i]
+	fmt.Println(nums)
+
 	quickSort1(nums, start, i-1)
-	quickSort1(nums, j+1, end)
+	quickSort1(nums, i+1, end)
 
 }
