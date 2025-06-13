43d6d230ca0cd8d7ada50a681396f56c4a4fc8d4
wangxuewei10 <wangxuewei15@jd.com>
2025-06-13 11:43:14 +0800

    add

--- leetcode/array/dailyTemperatures.go
+++ leetcode/array/dailyTemperatures.go
@@ -1,18 +1,34 @@
 package array
 
-func dailyTemperatures(temperatures []int) []int {
-	n := len(temperatures)
-	stack := make([]int, 0)
-	res := make([]int, n)
+/*
+给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
 
-	for i := 0; i < n; i++ {
+示例 1:
+
+输入: temperatures = [73,74,75,71,69,72,76,73]
+输出: [1,1,4,2,1,1,0,0]
+示例 2:
+
+输入: temperatures = [30,40,50,60]
+输出: [1,1,1,0]
+示例 3:
+
+输入: temperatures = [30,60,90]
+输出: [1,1,0]
+*/
+func dailyTemperatures(temperatures []int) []int {
+	res := make([]int, len(temperatures))
+	stack := make([]int, len(temperatures))
+
+	for i := 0; i < len(temperatures); i++ {
 		for len(stack) > 0 && temperatures[i] > temperatures[stack[len(stack)-1]] {
 			topIndex := stack[len(stack)-1]
-			stack = stack[:len(stack)-1]
 			res[topIndex] = i - topIndex
+			stack = stack[:len(stack)-1]
 		}
 		stack = append(stack, i)
 	}
 
 	return res
+
 }
