b249a8875fe3af57fb4094a4fb9595c4f357b1c0
wangxuewei10 <wangxuewei15@jd.com>
2026-01-29 10:47:25 +0800

    refactor(leetcode/array/twoSum.go): 优化 twoSum 函数的实现，使用 range 替代传统的 for 循环遍历数组，并简化返回值逻辑。

--- leetcode/array/twoSum.go
+++ leetcode/array/twoSum.go
@@ -2,16 +2,14 @@ package array
 
 // https://leetcode.cn/problems/two-sum/
 func twoSum(nums []int, target int) []int {
-	key := make(map[int]int)
-	for i := 0; i < len(nums); i++ {
-		key[nums[i]] = i
+	key := make(map[int]int, 0)
+	for i, v := range nums {
+		key[v] = i
 	}
-
-	for i := 0; i < len(nums); i++ {
-		if v, ok := key[target-nums[i]]; ok && i != v {
-			return []int{v, i}
+	for i, v := range nums {
+		if index, ok := key[target-v]; ok && index != i {
+			return []int{i, index}
 		}
-
 	}
-	return nil
+	return []int{-1, -1}
 }
